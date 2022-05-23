03c9e5739087106d88a3b449077f9287ffdb52e8
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-05-23 14:19:34 +0530

    home-done

---
+++ src/Components/Home/Tools.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import Tool from './Tool';
+
+const Tools = () => {
+    const [tools, setTools] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/tools')
+        .then(res => res.json())
+        .then(data => setTools(data))
+    })
+    return (
+        <div>
+         <h4 className="text-xl text-center">Tools</h4>
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-12">
+            {
+                tools.map(tool => <Tool 
+                key={tool._id}
+                tool={tool}
+                ></Tool>)
+            }
+         </div>
+        </div>
+    );
+};
+
+export default Tools;
+
+
+
+
+
